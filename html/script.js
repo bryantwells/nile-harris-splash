22054bc05d1503fd144c3c406f845475d24a09fd
Bryant Wells <mail@bryantwells.com>
2023-07-05 19:35:27 -0400

    l

--- html/script.js
+++ html/script.js
@@ -1,6 +1,12 @@
 const video = document.querySelector('video');
-const soundOnButton = document.querySelector('button[name="on"]');
-const soundOffButton = document.querySelector('button[name="off"]');
+
+const channels = [...document.querySelectorAll('channel')];
+const channelButtons = [...document.querySelectorAll('.button--channel')]
+
+const soundOnButton = document.querySelector('.button--soundOn');
+const soundOffButton = document.querySelector('.button--soundOff');
+
+video.src = channels.find((c) => c.id == 'channel-1').dataset.src;
 
 video.addEventListener('loadedmetadata', () => {
 	const duration = Math.floor(video.duration);
@@ -11,13 +17,23 @@ video.addEventListener('loadedmetadata', () => {
 });
 
 soundOnButton.addEventListener('click', () => {
-	soundOnButton.disabled = true;
-	soundOffButton.disabled = false;
+	soundOnButton.classList.add('is-active');
+	soundOffButton.classList.remove('is-active');
 	video.muted = false;
 });
 
 soundOffButton.addEventListener('click', () => {
-	soundOnButton.disabled = false;
-	soundOffButton.disabled = true;
+	soundOnButton.classList.remove('is-active');
+	soundOffButton.classList.add('is-active');
 	video.muted = true;
 });
+
+channelButtons.forEach((channelButton) => {
+	channelButton.addEventListener('click', () => {
+		video.src = channels.find((c) => c.id == `channel-${ channelButton.dataset.channel }`).dataset.src;
+		channelButtons.forEach((cb) => {
+			cb.classList.remove('is-active');
+		});
+		channelButton.classList.add('is-active');
+	});
+})
